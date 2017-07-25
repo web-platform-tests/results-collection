58b07cdb3e104770a89f632936958700a2d75c05
Jeff Carpenter <jeffcarp@chromium.org>
2017-07-25 10:07:27 -0700

    Address PR feedback

--- run/browsers_test.py
+++ run/browsers_test.py
@@ -1,7 +1,21 @@
-import unittest
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 import os
 import re
+import unittest
 
 
 VALID_PLATFORM_ID_REGEX = r"^[a-z0-9\-\.]+$"
@@ -16,11 +30,11 @@ REQUIRED_PLATFORM_FIELDS = {
 
 class TestBrowsers(unittest.TestCase):
 
-    def __init__(self, *args, **kwargs):
-        super(TestBrowsers, self).__init__(*args, **kwargs)
+    @classmethod
+    def setUpClass(cls):
         filepath = os.path.join(os.path.dirname(__file__), os.pardir, 'browsers.json')
         with open(filepath) as f:
-            self.browsers = json.load(f)
+            cls.browsers = json.load(f)
 
     def test_all_platforms_have_only_valid_characters(self):
         for platform_id, platform_info in self.browsers.iteritems():
@@ -33,7 +47,7 @@ class TestBrowsers(unittest.TestCase):
                 self.assertTrue(key in platform_info.keys(),
                     'Required field missing: %s (platform %s)' % (key, platform_id))
 
-                if REQUIRED_PLATFORM_FIELDS[key] != None:
+                if REQUIRED_PLATFORM_FIELDS[key] is not None:
                     self.assertTrue(platform_info[key] in REQUIRED_PLATFORM_FIELDS[key],
                         'Field has invalid value: %s (platform %s)' % (key, platform_id))
 
