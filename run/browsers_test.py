484958d451365f363141725cfb7e36b245ad9fef
Jeff Carpenter <jeffcarp@chromium.org>
2017-08-04 13:57:03 -0700

    Add Python linting (#71)

--- run/browsers_test.py
+++ run/browsers_test.py
@@ -28,28 +28,36 @@ REQUIRED_PLATFORM_FIELDS = {
     'os_version': None,
 }
 
+
 class TestBrowsers(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        filepath = os.path.join(os.path.dirname(__file__), os.pardir, 'browsers.json')
+        filepath = os.path.join(os.path.dirname(__file__),
+                                os.pardir, 'browsers.json')
         with open(filepath) as f:
             cls.browsers = json.load(f)
 
     def test_all_platforms_have_only_valid_characters(self):
         for platform_id, platform_info in self.browsers.items():
-            self.assertTrue(re.match(VALID_PLATFORM_ID_REGEX, platform_id),
-                'platform_id with invalid characters: %s' % platform_id)
+            self.assertTrue(
+                re.match(VALID_PLATFORM_ID_REGEX, platform_id),
+                'platform_id with invalid characters: %s' % platform_id
+            )
 
     def test_all_browsers_have_required_fields(self):
         for platform_id, platform_info in self.browsers.items():
             for key, valid_values in REQUIRED_PLATFORM_FIELDS.items():
-                self.assertTrue(key in platform_info.keys(),
-                    'Required field missing: %s (platform %s)' % (key, platform_id))
+                self.assertTrue(
+                    key in platform_info.keys(),
+                    'Required field missing: %s (platform %s)' %
+                    (key, platform_id))
 
                 if REQUIRED_PLATFORM_FIELDS[key] is not None:
-                    self.assertTrue(platform_info[key] in REQUIRED_PLATFORM_FIELDS[key],
-                        'Field has invalid value: %s (platform %s)' % (key, platform_id))
+                    self.assertTrue(
+                        platform_info[key] in REQUIRED_PLATFORM_FIELDS[key],
+                        'Field has invalid value: %s (platform %s)'
+                        % (key, platform_id))
 
     def test_no_two_browser_configs_are_equal(self):
         for first_platform_id, first_platform in self.browsers.items():
@@ -58,7 +66,9 @@ class TestBrowsers(unittest.TestCase):
                 in self.browsers.items()
                 if second_platform == first_platform
             ]
-            self.assertEqual(len(identical_platforms), 1,
+            self.assertEqual(
+                len(identical_platforms),
+                1,
                 'Duplicate platforms detected: %s' % identical_platforms)
 
 
