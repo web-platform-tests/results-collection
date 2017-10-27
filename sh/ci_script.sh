9c284d1e6de034de612e8ae2559fbcc595db74ed
Mark Dittmer <markdittmer@google.com>
2017-10-27 14:37:52 -0400

    Fix python unittest invocation so it picks up tests

--- sh/ci_script.sh
+++ sh/ci_script.sh
@@ -5,6 +5,6 @@ set -e
 SH_DIR=$(readlink -f $(dirname "$0"))
 
 "${SH_DIR}/exec.sh" pycodestyle --exclude=*_pb2.py .
-"${SH_DIR}/exec.sh" python -m unittest discover -p "./*_test.py"
+"${SH_DIR}/exec.sh" python -m unittest discover -p '*_test.py'
 "${SH_DIR}/exec.sh" go test -v ./...
 "${SH_DIR}/exec.sh" golint -set_exit_status
