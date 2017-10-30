3b1bf96ce9024be425d4c0d5e345c3b3292b8aba
Luke Bjerring <lukebjerring@chromium.org>
2017-10-30 15:47:31 -0400

    Correct regex and add unit tests

--- test_handler.go
+++ test_handler.go
@@ -34,7 +34,7 @@ import (
 // The browsers initially displayed to the user are defined in browsers.json.
 // The JSON property "initially_loaded" is what controls this.
 func testHandler(w http.ResponseWriter, r *http.Request) {
-    runSHA, err := getRunSHA(r)
+    runSHA, err := GetRunSHA(r)
     if err != nil {
         http.Error(w, "Invalid query params", http.StatusBadRequest)
         return
@@ -98,18 +98,20 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func getRunSHA(r *http.Request) (runSHA string, err error) {
+// GetRunSHA parses and validates the 'sha' param for the request.
+// It returns "latest" by default (and in error cases).
+func GetRunSHA(r *http.Request) (runSHA string, err error) {
     // Get the SHA for the run being loaded (the first part of the path.)
     runSHA = "latest"
     params, err := url.ParseQuery(r.URL.RawQuery)
     if err != nil {
-        return
+        return runSHA, err
     }
 
     runParam := params.Get("sha")
-    regex := regexp.MustCompile("[0-f]{10}")
+    regex := regexp.MustCompile("[0-9a-fA-F]{10}")
     if regex.MatchString(runParam) {
         runSHA = runParam
     }
-    return
+    return runSHA, err
 }
---
+++ test_handler_test.go
@@ -0,0 +1,57 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wptdashboard
+
+import (
+  "net/http/httptest"
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestGetRunSHA(t *testing.T) {
+  r := httptest.NewRequest("GET", "http://wpt.fyi/", nil)
+  runSHA, err := GetRunSHA(r)
+  assert.Nil(t, err)
+  assert.Equal(t, "latest", runSHA)
+}
+
+func TestGetRunSHA_2(t *testing.T) {
+  sha := "0123456789"
+  r := httptest.NewRequest("GET", "http://wpt.fyi/?sha=" + sha, nil)
+  runSHA, err := GetRunSHA(r)
+  assert.Nil(t, err)
+  assert.Equal(t, sha, runSHA)
+}
+
+func TestGetRunSHA_BadRequest(t *testing.T) {
+  r := httptest.NewRequest("GET", "http://wpt.fyi/?sha=%zz", nil)
+  runSHA, err := GetRunSHA(r)
+  assert.NotNil(t, err)
+  assert.Equal(t, "latest", runSHA)
+}
+
+func TestGetRunSHA_NonSHA(t *testing.T) {
+  r := httptest.NewRequest("GET", "http://wpt.fyi/?sha=123", nil)
+  runSHA, err := GetRunSHA(r)
+  assert.Nil(t, err)
+  assert.Equal(t, "latest", runSHA)
+}
+
+func TestGetRunSHA_NonSHA_2(t *testing.T) {
+  r := httptest.NewRequest("GET", "http://wpt.fyi/?sha=zapper0123", nil)
+  runSHA, err := GetRunSHA(r)
+  assert.Nil(t, err)
+  assert.Equal(t, "latest", runSHA)
+}
