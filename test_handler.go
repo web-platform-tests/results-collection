1455a7467d5f377254469ae3decfa0a5fb91e806
Luke Bjerring <lukebjerring@users.noreply.github.com>
2017-10-30 20:05:00 -0400

    Add support for ?run=[SHA] (#185)

--- test_handler.go
+++ test_handler.go
@@ -22,6 +22,8 @@ import (
 
     "google.golang.org/appengine"
     "google.golang.org/appengine/datastore"
+    "net/url"
+    "regexp"
 )
 
 // This handler is responsible for all pages that display test results.
@@ -32,9 +34,14 @@ import (
 // The browsers initially displayed to the user are defined in browsers.json.
 // The JSON property "initially_loaded" is what controls this.
 func testHandler(w http.ResponseWriter, r *http.Request) {
+    runSHA, err := GetRunSHA(r)
+    if err != nil {
+        http.Error(w, "Invalid query params", http.StatusBadRequest)
+        return
+    }
+
     ctx := appengine.NewContext(r)
     var bytes []byte
-    var err error
     var browsers map[string]Browser
 
     if bytes, err = ioutil.ReadFile("browsers.json"); err != nil {
@@ -61,6 +68,9 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
     for _, browserName := range browserNames {
         var testRunResults []TestRun
         query := baseQuery.Filter("BrowserName =", browserName)
+        if runSHA != "" && runSHA != "latest" {
+            query = query.Filter("Revision =", runSHA)
+        }
         if _, err := query.GetAll(ctx, &testRunResults); err != nil {
             http.Error(w, err.Error(), http.StatusInternalServerError)
             return
@@ -73,10 +83,35 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-    testRunsJSON := string(testRunsBytes)
 
-    if err := templates.ExecuteTemplate(w, "index.html", testRunsJSON); err != nil {
+    data := struct {
+        TestRuns string
+        SHA      string
+    }{
+        string(testRunsBytes),
+        runSHA,
+    }
+
+    if err := templates.ExecuteTemplate(w, "index.html", data); err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+}
+
+// GetRunSHA parses and validates the 'sha' param for the request.
+// It returns "latest" by default (and in error cases).
+func GetRunSHA(r *http.Request) (runSHA string, err error) {
+    // Get the SHA for the run being loaded (the first part of the path.)
+    runSHA = "latest"
+    params, err := url.ParseQuery(r.URL.RawQuery)
+    if err != nil {
+        return runSHA, err
+    }
+
+    runParam := params.Get("sha")
+    regex := regexp.MustCompile("[0-9a-fA-F]{10}")
+    if regex.MatchString(runParam) {
+        runSHA = runParam
+    }
+    return runSHA, err
 }
