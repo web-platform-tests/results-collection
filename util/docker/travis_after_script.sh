fdc86e33dab6b863bfe4d4d0a553c8acbec522a5
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-11-08 04:15:39 -0500

    Use same instance name in dev and Travis so that exec scripts will work

--- util/docker/travis_after_script.sh
+++ util/docker/travis_after_script.sh
@@ -8,8 +8,8 @@ WPTDASHBOARD_DIR=${WPTDASHBOARD_DIR:-$(readlink -f "${DOCKER_DIR}/../..")}
 
 cd "${WPTDASHBOARD_DIR}"
 
-info "Stopping and removing docker instance 'wpt-travis-instance'"
-docker stop wptd-travis-instance && docker rm wptd-travis-instance
+info "Stopping and removing docker instance 'wpt-dev-instance'"
+docker stop wptd-dev-instance && docker rm wptd-dev-instance
 DOCKER_STATUS=${?}
 if [ "${DOCKER_STATUS}" != "0" ]; then
   error "Docker stop-and-remove failed."
--- util/docker/travis_before_install.sh
+++ util/docker/travis_before_install.sh
@@ -10,5 +10,5 @@ cd "${WPTDASHBOARD_DIR}"
 docker build -t wptd-dev -f Dockefile.dev .
 docker run -d -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
     -v "$(pwd)":/wptdashboard -u $(id -u $USER):$(id -g $USER) \
-    --name wptd-travis-instance wptd-dev /wptdashboard/sh/watch.sh
+    --name wptd-dev-instance wptd-dev /wptdashboard/sh/watch.sh
 docker ps -a
