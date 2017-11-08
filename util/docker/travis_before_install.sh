2fed28cd45d49e446a5ea9e3a621e8caf1959f0e
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-11-08 03:56:29 -0500

    Fix up travis_before_install

--- util/docker/travis_before_install.sh
+++ util/docker/travis_before_install.sh
@@ -7,8 +7,8 @@ WPTDASHBOARD_DIR=${WPTDASHBOARD_DIR:-$(readlink -f "${DOCKER_DIR}/../..")}
 
 cd "${WPTDASHBOARD_DIR}"
 
-docker build -t wptdashboard:0.1 .
+docker build -t wptd-dev -f Dockefile.dev .
 docker run -d -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
     -v "$(pwd)":/wptdashboard -u $(id -u $USER):$(id -g $USER) \
-    --name wptd-travis-instance wptdashboard:0.1 /wptdashboard/sh/watch.sh
+    --name wptd-travis-instance wptd-dev /wptdashboard/sh/watch.sh
 docker ps -a
