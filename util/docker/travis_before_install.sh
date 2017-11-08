3811bb5a895c742d9670ed915e6c32fb9a6b9686
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-11-08 04:34:17 -0500

    Updated watch.sh path in Travis script

--- util/docker/travis_before_install.sh
+++ util/docker/travis_before_install.sh
@@ -11,5 +11,5 @@ docker build -t wptd-base -f Dockerfile.base .
 docker build -t wptd-dev -f Dockerfile.dev .
 docker run -d -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
     -v "$(pwd)":/wptdashboard -u $(id -u $USER):$(id -g $USER) \
-    --name wptd-dev-instance wptd-dev /wptdashboard/sh/watch.sh
+    --name wptd-dev-instance wptd-dev /wptdashboard/util/docker/inner/watch.sh
 docker ps -a
