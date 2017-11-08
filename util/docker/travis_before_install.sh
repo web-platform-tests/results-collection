b457e0b10cb02511898886b13f53c0d5e8630c73
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-11-08 04:32:01 -0500

    Drop debugging output from Travis script and gcloud components update right after gcloud components install (it just uninstalls and reinstalls everything)

--- util/docker/travis_before_install.sh
+++ util/docker/travis_before_install.sh
@@ -7,9 +7,6 @@ WPTDASHBOARD_DIR=${WPTDASHBOARD_DIR:-$(readlink -f "${DOCKER_DIR}/../..")}
 
 cd "${WPTDASHBOARD_DIR}"
 
-echo "ls ${WPTDASHBOARD_DIR}"
-ls
-
 docker build -t wptd-base -f Dockerfile.base .
 docker build -t wptd-dev -f Dockerfile.dev .
 docker run -d -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
