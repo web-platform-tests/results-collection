20c6308f62f873712b958ae283cb9ce81fecfedb
Luke Bjerring <lukebjerring@users.noreply.github.com>
2017-11-10 10:12:13 -0500

    gofmt all files

--- models.go
+++ models.go
@@ -15,38 +15,38 @@
 package wptdashboard
 
 import (
-    "time"
+	"time"
 )
 
 // TestRun stores metadata for a test run (produced by run/run.py)
 type TestRun struct {
-    // Platform information
-    BrowserName     string          `json:"browser_name"`
-    BrowserVersion  string          `json:"browser_version"`
-    OSName          string          `json:"os_name"`
-    OSVersion       string          `json:"os_version"`
+	// Platform information
+	BrowserName    string `json:"browser_name"`
+	BrowserVersion string `json:"browser_version"`
+	OSName         string `json:"os_name"`
+	OSVersion      string `json:"os_version"`
 
-    // The first 10 characters of the SHA1 of the tested WPT revision
-    Revision        string          `json:"revision"`
+	// The first 10 characters of the SHA1 of the tested WPT revision
+	Revision string `json:"revision"`
 
-    // Results URL
-    ResultsURL      string          `json:"results_url"`
+	// Results URL
+	ResultsURL string `json:"results_url"`
 
-    CreatedAt       time.Time       `json:"created_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 // Browser holds objects that appear in browsers.json
 type Browser struct {
-    InitiallyLoaded bool            `json:"initially_loaded"`
-    CurrentlyRun    bool            `json:"currently_run"`
-    BrowserName     string          `json:"browser_name"`
-    BrowserVersion  string          `json:"browser_version"`
-    OSName          string          `json:"os_name"`
-    OSVersion       string          `json:"os_version"`
-    Sauce           bool            `json:"sauce"`
+	InitiallyLoaded bool   `json:"initially_loaded"`
+	CurrentlyRun    bool   `json:"currently_run"`
+	BrowserName     string `json:"browser_name"`
+	BrowserVersion  string `json:"browser_version"`
+	OSName          string `json:"os_name"`
+	OSVersion       string `json:"os_version"`
+	Sauce           bool   `json:"sauce"`
 }
 
 // Token is used for test result uploads.
 type Token struct {
-    Secret          string          `json:"secret"`
+	Secret string `json:"secret"`
 }
