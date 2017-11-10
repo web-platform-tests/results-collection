634dab614a6f747b7d0677e6a8236f5c0451d0d3
Luke Bjerring <lukebjerring@users.noreply.github.com>
2017-11-10 10:08:41 -0500

    Add latest prod runs to populate task (#192)
    * Mimic golang 1.7
    * Fix go lint
    * Fix indenting

--- populate_dev_data_handler.go
+++ populate_dev_data_handler.go
@@ -15,13 +15,18 @@
 package wptdashboard
 
 import (
-    "net/http"
-    "time"
+    "errors"
     "fmt"
+    "net/http"
+    "net/url"
+    "time"
 
     "google.golang.org/appengine"
     "google.golang.org/appengine/datastore"
+    "google.golang.org/appengine/urlfetch"
 )
+
+var errUseLastResponse = errors.New("net/http: use last response")
 
 // Create TestRun entities for local development and testing.
 // These point to JSON files stored in /static/.
@@ -31,41 +36,77 @@ func populateDevData(w http.ResponseWriter, r *http.Request) {
 
     devData := map[string]*TestRun{
         "dev-testrun-chrome-63": &TestRun{
-            BrowserName: "chrome",
+            BrowserName:    "chrome",
             BrowserVersion: "63.0",
-            OSName: "linux",
-            OSVersion: "3.16",
-            Revision: "b952881825",
-            ResultsURL: "/static/b952881825/chrome-63.0-linux-summary.json.gz",
-            CreatedAt: time.Now(),
+            OSName:         "linux",
+            OSVersion:      "3.16",
+            Revision:       "b952881825",
+            ResultsURL:     "/static/b952881825/chrome-63.0-linux-summary.json.gz",
+            CreatedAt:      time.Now(),
         },
         "dev-testrun-edge-15": &TestRun{
-            BrowserName: "edge",
+            BrowserName:    "edge",
             BrowserVersion: "15",
-            OSName: "windows",
-            OSVersion: "10",
-            Revision: "b952881825",
-            ResultsURL: "/static/b952881825/edge-15-windows-10-sauce-summary.json.gz",
-            CreatedAt: time.Now(),
+            OSName:         "windows",
+            OSVersion:      "10",
+            Revision:       "b952881825",
+            ResultsURL:     "/static/b952881825/edge-15-windows-10-sauce-summary.json.gz",
+            CreatedAt:      time.Now(),
         },
         "dev-testrun-firefox-57": &TestRun{
-            BrowserName: "firefox",
+            BrowserName:    "firefox",
             BrowserVersion: "57.0",
-            OSName: "linux",
-            OSVersion: "*",
-            Revision: "b952881825",
-            ResultsURL: "/static/b952881825/firefox-57.0-linux-summary.json.gz",
-            CreatedAt: time.Now(),
+            OSName:         "linux",
+            OSVersion:      "*",
+            Revision:       "b952881825",
+            ResultsURL:     "/static/b952881825/firefox-57.0-linux-summary.json.gz",
+            CreatedAt:      time.Now(),
         },
         "dev-testrun-safari-10": &TestRun{
-            BrowserName: "safari",
+            BrowserName:    "safari",
             BrowserVersion: "10",
-            OSName: "macos",
-            OSVersion: "10.12",
-            Revision: "b952881825",
-            ResultsURL: "/static/b952881825/safari-10-macos-10.12-sauce-summary.json.gz",
-            CreatedAt: time.Now(),
+            OSName:         "macos",
+            OSVersion:      "10.12",
+            Revision:       "b952881825",
+            ResultsURL:     "/static/b952881825/safari-10-macos-10.12-sauce-summary.json.gz",
+            CreatedAt:      time.Now(),
         },
+    }
+
+    // Get the redirects, but don't follow them. Mimics http.ErrUseLastResponse (golang v1.7)
+    client := urlfetch.Client(ctx)
+    client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+        return errUseLastResponse
+    }
+
+    for _, browserName := range []string{
+        "chrome",
+        "edge",
+        "firefox",
+        "safari",
+    } {
+        // TODO(lukebjerring): Move wpt.fyi base URL to constant.
+        jsonURL := "https://wpt.fyi/json?platform=" + browserName
+        resp, err := client.Head(jsonURL)
+
+        if urlError, ok := err.(*url.Error); !ok || urlError.Err != errUseLastResponse {
+            fmt.Fprintf(w, "Failed to fetch latest run for %s: %s\n", browserName, urlError.Error())
+            continue
+        }
+
+        latestURL, err := resp.Location()
+        if err != nil {
+            fmt.Fprintf(w, "Failed to read redirected location for %s: %s\n", jsonURL, err.Error())
+            continue
+        }
+
+        devData["prod-latest-"+browserName] = &TestRun{
+            BrowserName:    browserName,
+            BrowserVersion: "latest",
+            Revision:       "latest",
+            ResultsURL:     latestURL.String(),
+            CreatedAt:      time.Now(),
+        }
     }
 
     for key, testRun := range devData {
