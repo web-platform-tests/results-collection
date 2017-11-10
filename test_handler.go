190370b24119e7c2e1526809ca2fb4ea9ea01b50
Luke Bjerring <lukebjerring@users.noreply.github.com>
2017-11-10 17:08:12 -0500

    Serve 'latest' as complete run (#199)

--- test_handler.go
+++ test_handler.go
@@ -15,6 +15,7 @@
 package wptdashboard
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -22,6 +23,9 @@ import (
 	"net/url"
 	"regexp"
 	"sort"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 )
 
 // This handler is responsible for all pages that display test results.
@@ -32,7 +36,7 @@ import (
 // The browsers initially displayed to the user are defined in browsers.json.
 // The JSON property "initially_loaded" is what controls this.
 func testHandler(w http.ResponseWriter, r *http.Request) {
-	runSHA, err := GetRunSHA(r)
+	runSHA, err := ParseSHAParam(r)
 	if err != nil {
 		http.Error(w, "Invalid query params", http.StatusBadRequest)
 		return
@@ -44,6 +48,16 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+
+	ctx := appengine.NewContext(r)
+	// Make sure to show results for the same complete run (executed for all browsers).
+	if runSHA == "latest" {
+		runSHA, err = getLastCompleteRunSHA(ctx, browserNames)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 
 	for _, browserName := range browserNames {
@@ -70,9 +84,9 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// GetRunSHA parses and validates the 'sha' param for the request.
+// ParseSHAParam parses and validates the 'sha' param for the request.
 // It returns "latest" by default (and in error cases).
-func GetRunSHA(r *http.Request) (runSHA string, err error) {
+func ParseSHAParam(r *http.Request) (runSHA string, err error) {
 	// Get the SHA for the run being loaded (the first part of the path.)
 	runSHA = "latest"
 	params, err := url.ParseQuery(r.URL.RawQuery)
@@ -109,4 +123,38 @@ func GetBrowserNames() (browserNames []string, err error) {
 	}
 	sort.Strings(browserNames)
 	return browserNames, nil
+}
+
+// getLastCompleteRunSHA returns the SHA[0:10] for the most recent run that complete for all of the given browser names.
+func getLastCompleteRunSHA(ctx context.Context, browserNames []string) (sha string, err error) {
+	baseQuery := datastore.
+		NewQuery("TestRun").
+		Order("-CreatedAt").
+		Limit(100).
+		Project("Revision")
+
+	runSHAs := make(map[string]int)
+	for _, browser := range browserNames {
+		it := baseQuery.Filter("BrowserName = ", browser).Run(ctx)
+		for {
+			var testRun TestRun
+			_, err := it.Next(&testRun)
+			if err == datastore.Done {
+				break
+			}
+			if err != nil {
+				return "latest", err
+			}
+			if _, ok := runSHAs[testRun.Revision]; !ok {
+				runSHAs[testRun.Revision] = 1
+			} else {
+				sum := runSHAs[testRun.Revision] + 1
+				if sum == len(browserNames) {
+					return testRun.Revision, nil
+				}
+				runSHAs[testRun.Revision] = sum
+			}
+		}
+	}
+	return "latest", nil
 }
