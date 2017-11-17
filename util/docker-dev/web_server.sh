e7c3f62b7a794396f6c207f7231420b930bd0494
Luke Bjerring <lukebjerring@chromium.org>
2017-11-17 09:41:13 -0500

    Also forward the admin + api ports in web_server.sh

--- util/docker-dev/web_server.sh
+++ util/docker-dev/web_server.sh
@@ -7,6 +7,8 @@ DOCKER_DIR=$(dirname "$0")
 source "${DOCKER_DIR}/../logging.sh"
 WPTD_PATH=${WPTD_PATH:-"${DOCKER_DIR}/../.."}
 
+WPTD_CONTAINER_HOST=0.0.0.0
+
 info "Installing web server code dependencies"
 docker exec -u $(id -u $USER):$(id -g $USER) wptd-dev-instance make build
 DOCKER_STATUS="${?}"
@@ -16,4 +18,11 @@ if [ "${DOCKER_STATUS}" != "0" ]; then
 fi
 info "Starting web server. Port forwarded from wptd-dev-instance: 8080"
 docker exec -it -u $(id -u $USER):$(id -g $USER) wptd-dev-instance \
-    dev_appserver.py --host=0.0.0.0 .
+    dev_appserver.py \
+    --host $WPTD_CONTAINER_HOST \
+    --port=8080 \
+    --admin_host=$WPTD_CONTAINER_HOST \
+    --admin_port=8000 \
+    --api_host=$WPTD_CONTAINER_HOST \
+    --api_port=9999 \
+    .
