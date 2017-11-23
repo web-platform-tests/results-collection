75e298d0010c02d3ebc9921ba11b49c3cb2aba15
Luke Bjerring <lukebjerring@chromium.org>
2017-11-23 13:39:15 -0500

    Add test for empty day

--- run/shas_test.py
+++ run/shas_test.py
@@ -29,12 +29,21 @@ wptd_dir = os.path.join(here, '../')
 class TestSHAFinder(unittest.TestCase):
 
     def test_nov_21st(self):
+        # ~5 commits that day, ensure first is result.
         logger = mock.Mock(logging.Logger)
 
         sha_finder = shas.SHAFinder(logger, date(2017, 11, 21))
 
         self.assertEqual('46060eb2c33de6101bc6930bf5e34f794aa9f996',
                          sha_finder.get_todays_sha(wptd_dir))
+
+    def test_nov_18th(self):
+        # No commits that day, ensure empty result.
+        logger = mock.Mock(logging.Logger)
+
+        sha_finder = shas.SHAFinder(logger, date(2017, 11, 18))
+
+        self.assertEqual('', sha_finder.get_todays_sha(wptd_dir))
 
 
 if __name__ == '__main__':
