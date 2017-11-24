b3205ff23555cfc2f2bd1d8cbea96d1542ff1206
Luke Bjerring <lukebjerring@users.noreply.github.com>
2017-11-24 10:42:01 -0500

    Add IsBrowserName util method, and test it. (#291)
    * Run gofmt
    
    * Use IsBrowserName from getBrowserName param parse
    
    * Update method doc

--- util.go
+++ util.go
@@ -21,7 +21,10 @@ import (
 )
 
 var browsers map[string]Browser
-var browserNames []string
+
+// No 'set' type in Go, so use map instead.
+var browserNames map[string]bool
+var browserNamesAlphabetical []string
 
 // GetBrowsers loads, parses and returns the set of names of browsers
 // which are to be included (flagged as initially_loaded in the JSON).
@@ -44,20 +47,34 @@ func GetBrowsers() (map[string]Browser, error) {
 // GetBrowserNames returns an alphabetically-ordered array of the names
 // of the browsers returned by GetBrowsers.
 func GetBrowserNames() ([]string, error) {
-	if browserNames != nil {
-		return browserNames, nil
-	}
+	return browserNamesAlphabetical, nil
+}
 
+// IsBrowserName determines whether the given name string is a valid browser name.
+// Used for validating user-input params for browsers.
+func IsBrowserName(name string) bool {
+	if browserNames == nil {
+		if err := loadBrowserNames(); err != nil {
+			return false
+		}
+	}
+	_, ok := browserNames[name]
+	return ok
+}
+
+func loadBrowserNames() error {
 	var browsers map[string]Browser
 	var err error
 	if browsers, err = GetBrowsers(); err != nil {
-		return nil, err
+		return err
 	}
+	browserNames = make(map[string]bool)
 	for _, browser := range browsers {
 		if browser.InitiallyLoaded {
-			browserNames = append(browserNames, browser.BrowserName)
+			browserNamesAlphabetical = append(browserNamesAlphabetical, browser.BrowserName)
+			browserNames[browser.BrowserName] = true
 		}
 	}
-	sort.Strings(browserNames)
-	return browserNames, nil
+	sort.Strings(browserNamesAlphabetical)
+	return nil
 }
