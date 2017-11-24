6724132f549119fa566301ba361080062cbe6f6b
Luke Bjerring <lukebjerring@chromium.org>
2017-11-24 16:25:46 -0500

    Add support for max-count param in /api/runs

--- params_test.go
+++ params_test.go
@@ -140,3 +140,31 @@ func TestParseBrowsersParam_MultiBrowserParam_AllInvalid(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Empty(t, browsers)
 }
+
+func TestParseMaxCountParam(t *testing.T) {
+	r := httptest.NewRequest("GET", "http://wpt.fyi/?max-count=2", nil)
+	count, err := ParseMaxCountParam(r)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, count)
+}
+
+func TestParseMaxCountParam_Missing(t *testing.T) {
+	r := httptest.NewRequest("GET", "http://wpt.fyi/", nil)
+	count, err := ParseMaxCountParam(r)
+	assert.Nil(t, err)
+	assert.Equal(t, MaxCountDefaultValue, count)
+}
+
+func TestParseMaxCountParam_TooSmall(t *testing.T) {
+	r := httptest.NewRequest("GET", "http://wpt.fyi/?max-count=0", nil)
+	count, err := ParseMaxCountParam(r)
+	assert.Nil(t, err)
+	assert.Equal(t, MaxCountMinValue, count)
+}
+
+func TestParseMaxCountParam_TooLarge(t *testing.T) {
+	r := httptest.NewRequest("GET", "http://wpt.fyi/?max-count=123456789", nil)
+	count, err := ParseMaxCountParam(r)
+	assert.Nil(t, err)
+	assert.Equal(t, MaxCountMaxValue, count)
+}
