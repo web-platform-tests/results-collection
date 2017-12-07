070c3a570d0edd9646aedf16dc2c82a8852daba9
Mark Dittmer <markdittmer@google.com>
2017-12-07 15:10:14 -0500

    Nit: Fix typo

--- metrics/run/collect_metrics.go
+++ metrics/run/collect_metrics.go
@@ -133,7 +133,7 @@ func main() {
 	go func() {
 		defer wg.Done()
 		passRateMetric = compute.ComputePassRateMetric(len(runs),
-			&resultsById, compute.OkAndUnknonwOrPasses)
+			&resultsById, compute.OkAndUnknownOrPasses)
 	}()
 	for _, run := range runs {
 		go func(browserName string) {
@@ -142,7 +142,7 @@ func main() {
 			failuresMetrics[browserName] =
 				compute.ComputeBrowserFailureList(len(runs),
 					browserName, &resultsById,
-					compute.OkAndUnknonwOrPasses)
+					compute.OkAndUnknownOrPasses)
 		}(run.BrowserName)
 	}
 	wg.Wait()
