31e3f2caa9ffc194636044f447a15969bcd32c75
Rick Waldron <waldron.rick@gmail.com>
2017-12-21 09:48:55 -0500

    Fix lint/pycodestyle E302
    - ./run/run_summary.py:5:1: E302 expected 2 blank lines, found 1

--- run/run_summary.py
+++ run/run_summary.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+
 class TestRunSpec(object):
     def __init__(self, sha, platform):  # type: (str, str) -> None
         self.sha = sha
