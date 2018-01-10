71267e5fb68f561b31a99b4a29c37d7ae4fe5a17
Luke Bjerring <lukebjerring@chromium.org>
2018-01-10 14:42:39 -0500

    Add interop views

---
+++ webapp/interop_handler.go
@@ -0,0 +1,20 @@
+// Copyright 2017 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package webapp
+
+import (
+	"net/http"
+)
+
+// interopHandler handles the view of test results broken down by the
+// number of browsers for which the test passes.
+func interopHandler(w http.ResponseWriter, r *http.Request) {
+	// Empty struct placeholder.
+	data := struct{}{}
+	if err := templates.ExecuteTemplate(w, "interoperability.html", data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
