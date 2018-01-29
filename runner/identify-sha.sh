884e7734f8b30f8ce633cdaf297581a63db8d8b6
Matt Lee <mattl@bocoup.com>
2018-01-29 22:03:29 +0000

    Docs on how the index.html page works using SSI.

--- runner/identify-sha.sh
+++ runner/identify-sha.sh
@@ -5,6 +5,9 @@
 
 # Run this script in cron, and have it write to the
 # /var/www/html folder a file called sha
+#
+# You should have Options +Includes, mod_include and XBitHack on in
+# your <VirtualHost> for the htdocs/index.html page to work.
 
 sha="$(git ls-remote https://github.com/w3c/web-platform-tests.git | head -1 | cut -f 1 | cut -c 1-10)"
 
