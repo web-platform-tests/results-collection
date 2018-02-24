5a4d183851e159f655d468cda269bef9c62ea2be
Mike Pennisi <mike@mikepennisi.com>
2018-02-23 20:11:11 -0500

    Run tests as de-privileged user
    Executing long-running processes as a de-privileged user is a standard
    practice for defense-in-depth.

--- util/provision-run/files/browser-runner.sh
+++ util/provision-run/files/browser-runner.sh
@@ -2,7 +2,7 @@
 
 export BROWSER=
 
-if [ -f "/root/browser-is-running.txt" ]; then
+if [ -f "./browser-is-running.txt" ]; then
     echo "Not running, browser tests are running"
 else
 
@@ -12,14 +12,14 @@ else
     find /tmp -type f -name "*hromium*" -print0 | xargs -0 -r rm -f
     find /tmp -type f -name "*hrome*" -print0 | xargs -0 -r rm -f
     
-    touch /root/browser-is-running.txt
+    touch ./browser-is-running.txt
 
     #    rm -rf ~/wptdbuild/*
-    cd /root/wptdashboard/ && ./run/run.py $BROWSER \
+    cd ./wptdashboard/ && ./run/run.py $BROWSER \
       --upload --create-testrun --total-chunks 100 2>&1 | \
         tee browser-`date +%s`.log
 
-    rm /root/browser-is-running.txt
+    rm ./browser-is-running.txt
 
 fi
 
