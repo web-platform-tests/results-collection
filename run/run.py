13bedee056078900d96e787d0bde3dddaee6982b
Jonathon Kereliuk <kereliuk@chromium.org>
2018-03-01 11:02:09 -0500

    add command line flags for webrtc

--- run/run.py
+++ run/run.py
@@ -176,6 +176,11 @@ def main(platform_id, platform, args, config):
                 command.insert(5, args.path)
             if platform['browser_name'] == 'chrome':
                 command.extend(['--binary', browser_binary])
+
+                # temporary fix to allow WebRTC tests to call getUserMedia
+                command.extend(['--binary-arg=--use-fake-ui-for-media-stream'])
+                command.extend(['--binary-arg=--use-fake-device-for-media-stream'])
+                print(command)
             if platform['browser_name'] == 'firefox':
                 command.extend(['--install-browser', '--yes'])
                 command.append('--certutil-binary=certutil')
