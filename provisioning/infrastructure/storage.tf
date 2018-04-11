b3158b98c0af6ee035d18042d3319137f1780266
Mike Pennisi <mike@mikepennisi.com>
2018-04-10 21:37:09 -0400

    Formally manage Google Cloud Platform inventory
    Extend the project's Terraform configuration with descriptions of the
    required object stores as provided by Google Cloud Platform.

--- provisioning/infrastructure/storage.tf
+++ provisioning/infrastructure/storage.tf
@@ -6,3 +6,28 @@ resource "aws_ebs_volume" "build_master_database" {
     "Name" = "${var.name}-build-master-database"
   }
 }
+
+# The results collector uploads JSON-formatted WPT results to this bucket. The
+# https://wpt.fyi website currently retrieves files from this bucket directly,
+# necessitating CORS support.
+resource "google_storage_bucket" "results_store" {
+  name = "wptd"
+  location = "US"
+  storage_class = "MULTI_REGIONAL"
+
+  cors {
+    max_age_seconds = 86400
+    origin = ["*"]
+    method = ["GET", "HEAD"]
+  }
+}
+
+# The results collector installs experimental browser builds prior to
+# collecting results. In order to ensure failed collection attempts can be
+# re-tried at a later date (even after new browser builds have been published),
+# it uses the following bucket as a mirror for the installation artifacts.
+resource "google_storage_bucket" "browser_store" {
+  name = "browsers"
+  location = "US"
+  storage_class = "MULTI_REGIONAL"
+}
