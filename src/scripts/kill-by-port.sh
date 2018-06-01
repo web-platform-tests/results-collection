37b5f2fc5f0a9cb9b2b6b81333efb81b9a7a3b9f
Mike Pennisi <mike@mikepennisi.com>
2018-05-31 20:26:45 -0400

    PARTIAL: Begin to implement macOS/Safari worker

--- src/scripts/kill-by-port.sh
+++ src/scripts/kill-by-port.sh
@@ -8,21 +8,16 @@ if [ "$port" == "" ]; then
   exit 1
 fi
 
-pid=$(netstat -nlp 2>/dev/null | \
-  grep :$port | \
-  sed 's/^.*LISTEN\s*\([0-9]\+\).*/\1/')
+# Prefer `lsof` over `netstat` for portability between GNU and BSD
+# implementations
+pid=$(lsof -n -i :$port | \
+  grep LISTEN | \
+  awk '{ print $2 }')
 
 
 if [ "$pid" == "" ]; then
-  echo No process bound to port $port >&2
+  echo The current user is not running a process bound to port $port >&2
   exit 0
-fi
-
-echo $pid | grep '^[0-9]\+$' > /dev/null
-
-if [ $? != '0' ]; then
-  echo Unable to kill process bound to $port >&2
-  exit 1
 fi
 
 kill -9 $pid > /dev/null 2> /dev/null
