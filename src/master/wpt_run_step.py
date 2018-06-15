cfc0e64fcfcc92afa8eae3a5f006d931d39edd72
Mike Pennisi <mike@mikepennisi.com>
2018-06-14 20:57:45 -0400

    Disable SSL bumping in Sauce Labs
    This feature of the Sauce Connect binary interferes with all tests
    involving WebSockets. Because disabling the feature may slow the results
    collection process, this change is being made provisionally.

--- src/master/wpt_run_step.py
+++ src/master/wpt_run_step.py
@@ -68,7 +68,7 @@ class WptRunStep(steps.ShellCommand):
                 '--sauce-user', 'wpt-%s' % workername,
                 '--sauce-key', key,
                 '--sauce-tunnel-id', properties.getProperty('workername'),
-                '--sauce-connect-binary', 'sc',
+                '--sauce-connect-binary', 'sc-no-ssl-bump-domains',
                 '--no-restart-on-unexpected',
                 '--run-by-dir', '3'
             ])
