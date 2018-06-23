e2ee870f25090f5438bea72c670c6269789c2ae0
Mike Pennisi <mike@mikepennisi.com>
2018-06-22 21:18:18 -0400

    Revert "Disable SSL bumping in Sauce Labs"
    This reverts commit cfc0e64fcfcc92afa8eae3a5f006d931d39edd72.

--- src/master/wpt_run_step.py
+++ src/master/wpt_run_step.py
@@ -67,7 +67,7 @@ class WptRunStep(steps.ShellCommand):
                 '--sauce-user', 'wpt-%s' % workername,
                 '--sauce-key', key,
                 '--sauce-tunnel-id', properties.getProperty('workername'),
-                '--sauce-connect-binary', 'sc-no-ssl-bump-domains',
+                '--sauce-connect-binary', 'sc',
                 '--no-restart-on-unexpected',
                 '--run-by-dir', '3'
             ])
