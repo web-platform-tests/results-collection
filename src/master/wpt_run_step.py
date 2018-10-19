27c22db74aefb3e1ae93d34a76c7c9502ce48b64
Mike Pennisi <mike@mikepennisi.com>
2018-10-19 17:21:17 -0400

    Allow restart following test failure on Sauce Labs
    Because this project has recently stopped collecting results from Apple
    Safari via the Sauce Labs service, additional resources are available
    for testing Microsoft Edge in the environment. Make use of these
    resources by restarting the browser in response to any test failure or
    error.

--- src/master/wpt_run_step.py
+++ src/master/wpt_run_step.py
@@ -75,7 +75,6 @@ class WptRunStep(steps.ShellCommand):
                 '--sauce-tunnel-id', properties.getProperty('workername'),
                 '--sauce-connect-binary', 'sc-wrapper',
                 '--sauce-init-timeout', '45',
-                '--no-restart-on-unexpected',
                 '--run-by-dir', '3'
             ])
         else:
