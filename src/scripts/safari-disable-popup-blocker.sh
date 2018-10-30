49ea7fa20b67e34cfcb95f6ffa73797156e9c725
Charles Vazac <cvazac@gmail.com>
2018-10-29 20:29:26 -0500

    add removal TODO

--- src/scripts/safari-disable-popup-blocker.sh
+++ src/scripts/safari-disable-popup-blocker.sh
@@ -32,6 +32,8 @@ fi
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool true
 
 # turn on experimental features
+
+# TODO(cvazac) Remove this if/when Server-Timing is enabled by default in Safari
 defaults write com.apple.Safari ExperimentalServerTimingEnabled -bool true
 
 echo Closing all instances of the application to ensure the changes
