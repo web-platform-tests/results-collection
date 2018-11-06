f772c640e65435ecc1f61b16c852784168a722bc
Charles Vazac <cvazac@gmail.com>
2018-11-06 13:28:54 -0600

    fix errant double paste

--- src/scripts/safari-enable-experimental-features.sh
+++ src/scripts/safari-enable-experimental-features.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # turn on experimental features
-safari-enable-experimental-features.shsafari-enable-experimental-features.sh
+
 # TODO(cvazac) Remove this if/when Server-Timing is enabled by default in Safari
 defaults write com.apple.Safari ExperimentalServerTimingEnabled -bool true
 
