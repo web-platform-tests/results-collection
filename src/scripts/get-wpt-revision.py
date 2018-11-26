e24b685a129460713095913e718724d21a6ab36f
Mike Pennisi <mike@mikepennisi.com>
2018-11-26 18:13:59 -0500

    Tolerate temporary unavailability of wpt.fyi
    This project recently experienced a failure because wpt.fyi was
    temporarily unavailable [1]. It has since been confirmed that
    intermittent outages are expected [2], so the system should tolerate the
    condition by retrying failed requests for a short period.
    
    [1] https://github.com/web-platform-tests/results-collection/issues/632
    [2] https://github.com/web-platform-tests/wpt.fyi/issues/802

--- src/scripts/get-wpt-revision.py
+++ src/scripts/get-wpt-revision.py
@@ -8,7 +8,12 @@ import argparse
 import contextlib
 import httplib
 import json
+import logging
+import time
 import urlparse
+
+
+logger = logging.getLogger(__name__)
 
 
 @contextlib.contextmanager
@@ -32,17 +37,42 @@ def request(method, url):
     conn.close()
 
 
-def main(interval):
+@contextlib.contextmanager
+def get_with_retry(url, interval, timeout):
+    try:
+        with request('GET', url) as response:
+            status = response.status
+            message = 'HTTP Error %s: %s' % (status, response.reason)
+
+            if status >= 200 and status < 300:
+                yield response
+                return
+    except Exception as exception:
+        message = str(exception)
+
+    remaining = timeout - interval
+
+    if remaining <= 0:
+        logger.warn('Retry timeout exceeded.')
+        raise Exception(message)
+
+    logging.warn(message)
+    logging.warn('Retrying in %s seconds...' % (interval,))
+
+    time.sleep(interval)
+
+    with request_with_retry(method, url, interval, remaining) as response:
+        yield response
+
+
+def main(interval, retry_interval, retry_timeout):
     '''Query the wpt.fyi "revision announcer" for the latest revision of
     interest as published at the specified interval. Documentation available
     online at https://github.com/web-platform-tests/wpt.fyi/'''
 
-    with request('GET', 'https://wpt.fyi/api/revisions/latest') as response:
-        if response.status < 200 or response.status >= 300:
-            raise Exception(
-                'HTTP Error %s: %s' % (response.status, response.reason)
-            )
+    url = 'https://wpt.fyi/api/revisions/latest'
 
+    with get_with_retry(url, retry_interval, retry_timeout) as response:
         body = response.read()
 
         try:
@@ -64,6 +94,18 @@ def main(interval):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument('--retry-interval',
+                        type=int,
+                        default=60,
+                        help='''Duration in seconds to wait between retrying
+                            failed requests to wpt.fyi'''
+                        )
+    parser.add_argument('--retry-timeout',
+                        type=int,
+                        default=60 * 1000,
+                        help='''Duration in seconds to wait before cancelling
+                            attempts to retry failed requests'''
+                        )
     parser.add_argument('interval',
                         choices=('hourly', 'two_hourly', 'six_hourly',
                                  'twelve_hourly', 'daily', 'weekly'))
